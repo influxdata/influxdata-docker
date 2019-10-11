90f219e42a920d2e05fe4d744a78b51bcb98f556
gitirabassi <giacomo@tirabassi.eu>
2019-10-11 11:27:45 +0200

    feat(telegraf): telegraf config into environment variable

--- telegraf/1.12/entrypoint.sh
+++ telegraf/1.12/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+
+# You can also set the TELEGRAF_CONFIG_CONTENT environment variable to pass some
+# Telegraf configuration without having to bind any volumes.
+# This is very usefull within kuberentes: the problem of using configmaps is that 
+# a change to a config doesn't restart telegraf
+if [ -n "$TELEGRAF_CONFIG_CONTENT" ]; then
+    echo "$TELEGRAF_CONFIG_CONTENT" > "/etc/telegraf/telegraf.conf"
+fi
+
 if [ "${1:0:1}" = '-' ]; then
     set -- telegraf "$@"
 fi
