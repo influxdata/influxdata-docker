9485bc342ef0a069aab59cdaff4459ce5d7698b2
Sebastian Spaink <sspaink@influxdata.com>
2021-11-08 16:09:29 -0600

    fix: use su

--- telegraf/nightly/entrypoint.sh
+++ telegraf/nightly/entrypoint.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 if [ "${1:0:1}" = '-' ]; then
     set -- telegraf "$@"
 fi
 
-exec "$@"
+if [ "$(id -u)" -ne 0 ]; then
+    exec "$@"
+else
+    exec su-exec telegraf "$@"
+fi
