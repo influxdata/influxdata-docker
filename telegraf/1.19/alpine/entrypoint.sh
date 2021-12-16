3411461f62c563c748d66b7249b1c4d0a1562c25
Joshua Powers <powersj@influxdata.com>
2021-12-16 08:14:47 -0700

    fix: telegraf setcap command (#558)

--- telegraf/1.19/alpine/entrypoint.sh
+++ telegraf/1.19/alpine/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-# Allow telegraf to send ICMP packets and bind to privliged ports
-setcap cap_net_raw,cap_net_bind_service+ep /usr/bin/telegraf
-
 if [ "${1:0:1}" = '-' ]; then
     set -- telegraf "$@"
 fi
@@ -11,5 +8,8 @@ fi
 if [ "$(id -u)" -ne 0 ]; then
     exec "$@"
 else
+    # Allow telegraf to send ICMP packets and bind to privliged ports
+    setcap cap_net_raw,cap_net_bind_service+ep /usr/bin/telegraf
+
     exec su-exec telegraf "$@"
 fi
--- telegraf/1.20/entrypoint.sh
+++ telegraf/1.20/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Allow telegraf to send ICMP packets and bind to privliged ports
-setcap cap_net_raw,cap_net_bind_service+ep /usr/bin/telegraf
-
 if [ "${1:0:1}" = '-' ]; then
     set -- telegraf "$@"
 fi
@@ -11,5 +8,8 @@ fi
 if [ $EUID -ne 0 ]; then
     exec "$@"
 else
+    # Allow telegraf to send ICMP packets and bind to privliged ports
+    setcap cap_net_raw,cap_net_bind_service+ep /usr/bin/telegraf
+
     exec setpriv --reuid telegraf --init-groups "$@"
 fi
