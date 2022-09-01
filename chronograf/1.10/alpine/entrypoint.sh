445b1f6af8384e20c0827c18badbbbd71cccb96b
Brandon Pfeifer <bpfeifer@influxdata.com>
2022-09-01 10:28:13 -0400

    chore: release chronograf 1.10.0

---
+++ chronograf/1.10/alpine/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+if [ "${1:0:1}" = '-' ]; then
+    set -- chronograf "$@"
+fi
+
+if [ "$1" = 'chronograf' ]; then
+  export BOLT_PATH=${BOLT_PATH:-/var/lib/chronograf/chronograf-v1.db}
+fi
+
+exec "$@"
