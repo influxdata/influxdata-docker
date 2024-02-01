26329bb7d08433c55e585bc2eedeb9f4515258cd
Josh Powers <powersj@fastmail.com>
2024-02-01 14:48:40 -0700

    feat: restrict capabilities for telegraf

--- telegraf/nightly/entrypoint.sh
+++ telegraf/nightly/entrypoint.sh
@@ -8,9 +8,6 @@ fi
 if [ $EUID -ne 0 ]; then
     exec "$@"
 else
-    # Allow telegraf to send ICMP packets and bind to privliged ports
-    setcap cap_net_raw,cap_net_bind_service+ep /usr/bin/telegraf || echo "Failed to set additional capabilities on /usr/bin/telegraf"
-
     export HOME=$(getent passwd telegraf | cut -d : -f 6)
-    exec setpriv --reuid telegraf --regid telegraf --groups telegraf "$@"
+    exec setpriv --reuid telegraf --regid telegraf --groups telegraf --bounding-set=-all,+net_raw,+net_bind_service --inh-caps=-all,+net_raw,+net_bind_service "$@"
 fi
