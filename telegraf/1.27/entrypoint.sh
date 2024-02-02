a5f61a9eb2c3b64116022fcad4a4f2e754fed346
Josh Powers <powersj@fastmail.com>
2024-02-02 06:52:59 -0700

    update based on reviews

--- telegraf/1.27/entrypoint.sh
+++ telegraf/1.27/entrypoint.sh
@@ -9,5 +9,12 @@ if [ $EUID -ne 0 ]; then
     exec "$@"
 else
     export HOME=$(getent passwd telegraf | cut -d : -f 6)
-    exec setpriv --reuid telegraf --regid telegraf --groups telegraf --bounding-set=-all,+net_raw,+net_bind_service --inh-caps=-all,+net_raw,+net_bind_service "$@"
+    # attempt to set the fscaps on the telegraf binary then limit telegraf to only
+    # being able to inherit those capabilities
+    if setcap cap_net_raw,cap_net_bind_service+ep /usr/bin/telegraf ; then
+        exec setpriv --reuid telegraf --regid telegraf --groups telegraf --bounding-set=-all,+net_raw,+net_bind_service --inh-caps=-all,+net_raw,+net_bind_service "$@"
+    else
+        echo "Failed to set additional capabilities on /usr/bin/telegraf"
+        exec setpriv --reuid telegraf --regid telegraf --groups telegraf "$@"
+    fi
 fi
